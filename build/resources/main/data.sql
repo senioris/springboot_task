a520e525fdd1477e36415dc3e9482aab320592e3
senioris <3rd.hierarchy@gmail.com>
2024-01-08 12:04:03 +0000

    Delete build folder

--- build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tasks (title) VALUES ('task1')
-INSERT INTO tasks (title) VALUES ('task2')
-INSERT INTO tasks (title) VALUES ('task3')
-INSERT INTO tasks (title) VALUES ('task4')
-INSERT INTO tasks (title) VALUES ('task5')
